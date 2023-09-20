1a480e35d96d7fb11639dc50fa8e1bd621557f52
anshen <jiangrenjie.jrj@alibaba-inc.com>
2023-09-20 11:43:40 +0800

    联网键盘模式接收一键一发性能优化

--- app/src/main/java/ly/pp/justpiano3/handler/android/OLPlayKeyboardRoomHandler.java
+++ app/src/main/java/ly/pp/justpiano3/handler/android/OLPlayKeyboardRoomHandler.java
@@ -3,14 +3,13 @@ package ly.pp.justpiano3.handler.android;
 import android.app.Activity;
 import android.os.Handler;
 import android.os.Message;
-
-import java.lang.ref.WeakReference;
-
 import ly.pp.justpiano3.activity.OLPlayKeyboardRoom;
 import ly.pp.justpiano3.adapter.KeyboardPlayerImageAdapter;
 import ly.pp.justpiano3.entity.User;
 import ly.pp.justpiano3.utils.ColorUtil;
 import ly.pp.justpiano3.utils.SoundEngineUtil;
+
+import java.lang.ref.WeakReference;
 
 public final class OLPlayKeyboardRoomHandler extends Handler {
     private final WeakReference<Activity> weakReference;
@@ -60,36 +59,44 @@ public final class OLPlayKeyboardRoomHandler extends Handler {
                                 olPlayKeyboardRoom.playerGrid.setAdapter(new KeyboardPlayerImageAdapter(olPlayKeyboardRoom.playerList, olPlayKeyboardRoom));
                             }
                         }
-                        olPlayKeyboardRoom.receiveThreadPool.execute(() -> {
+                        int totalIntervalTime = 0;
+                        for (int i = 1; i < notes.length; i += 3) {
+                            totalIntervalTime += notes[i];
+                        }
+                        // 无间隔时间，不用启动子线程，提升一键一发的执行效率
+                        if (totalIntervalTime == 0) {
                             for (int i = 1; i < notes.length; i += 3) {
-                                int intervalTime = notes[i];
-                                if (intervalTime > 0) {
-                                    try {
-                                        Thread.sleep(intervalTime);
-                                    } catch (Exception e) {
-                                        e.printStackTrace();
-                                    }
-                                }
                                 if (!olPlayKeyboardRoom.olKeyboardStates[roomPositionSub1].isMuted()) {
                                     SoundEngineUtil.playSound(notes[i + 1], notes[i + 2]);
                                 }
-                                int finalI = i;
-                                olPlayKeyboardRoom.runOnUiThread(() -> {
-                                    if (notes[finalI + 2] > 0) {
-                                        olPlayKeyboardRoom.keyboardView.fireKeyDown(notes[finalI + 1], notes[finalI + 2],
-                                                user.getKuang() == 0 ? null : ColorUtil.getKuangColorByKuangIndex(olPlayKeyboardRoom, user.getKuang()));
-                                    } else {
-                                        olPlayKeyboardRoom.keyboardView.fireKeyUp(notes[finalI + 1]);
-                                    }
-                                });
+                                handleKeyboardView(olPlayKeyboardRoom, notes, user, i);
                             }
-                            olPlayKeyboardRoom.runOnUiThread(() -> {
+                            if (olPlayKeyboardRoom.playerGrid.getAdapter() != null && olPlayKeyboardRoom.olKeyboardStates[roomPositionSub1].isPlaying()) {
+                                olPlayKeyboardRoom.olKeyboardStates[roomPositionSub1].setPlaying(false);
+                                ((KeyboardPlayerImageAdapter) (olPlayKeyboardRoom.playerGrid.getAdapter())).notifyDataSetChanged();
+                            }
+                        } else {
+                            olPlayKeyboardRoom.receiveThreadPool.execute(() -> {
+                                for (int i = 1; i < notes.length; i += 3) {
+                                    if (notes[i] > 0) {
+                                        try {
+                                            Thread.sleep(notes[i]);
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                    if (!olPlayKeyboardRoom.olKeyboardStates[roomPositionSub1].isMuted()) {
+                                        SoundEngineUtil.playSound(notes[i + 1], notes[i + 2]);
+                                    }
+                                    int finalI = i;
+                                    olPlayKeyboardRoom.runOnUiThread(() -> handleKeyboardView(olPlayKeyboardRoom, notes, user, finalI));
+                                }
                                 if (olPlayKeyboardRoom.playerGrid.getAdapter() != null && olPlayKeyboardRoom.olKeyboardStates[roomPositionSub1].isPlaying()) {
                                     olPlayKeyboardRoom.olKeyboardStates[roomPositionSub1].setPlaying(false);
-                                    ((KeyboardPlayerImageAdapter) (olPlayKeyboardRoom.playerGrid.getAdapter())).notifyDataSetChanged();
+                                    olPlayKeyboardRoom.runOnUiThread(() -> ((KeyboardPlayerImageAdapter) (olPlayKeyboardRoom.playerGrid.getAdapter())).notifyDataSetChanged());
                                 }
                             });
-                        });
+                        }
                     });
                     return;
                 case 8:
@@ -143,6 +150,15 @@ public final class OLPlayKeyboardRoomHandler extends Handler {
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private void handleKeyboardView(OLPlayKeyboardRoom olPlayKeyboardRoom, byte[] notes, User user, int i) {
+        if (notes[i + 2] > 0) {
+            olPlayKeyboardRoom.keyboardView.fireKeyDown(notes[i + 1], notes[i + 2],
+                    user.getKuang() == 0 ? null : ColorUtil.getKuangColorByKuangIndex(olPlayKeyboardRoom, user.getKuang()));
+        } else {
+            olPlayKeyboardRoom.keyboardView.fireKeyUp(notes[i + 1]);
         }
     }
 }
