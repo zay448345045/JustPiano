4e0a0faaf93032d263f5b71ce5c338bfb2029644
anshen <jiangrenjie.jrj@alibaba-inc.com>
2023-10-09 17:24:31 +0800

    解决点击进入已满和弹奏中房间后可能进入其他房间的问题

--- app/src/main/java/ly/pp/justpiano3/enums/RoomModeEnum.java
+++ app/src/main/java/ly/pp/justpiano3/enums/RoomModeEnum.java
@@ -7,18 +7,20 @@ package ly.pp.justpiano3.enums;
  **/
 public enum RoomModeEnum {
 
-    NORMAL(0, "普通模式"),
-    TEAM(1, "组队模式"),
-    COUPLE(2, "双人模式"),
-    KEYBOARD(3, "键盘模式"),
+    NORMAL(0, "普通模式", "普通"),
+    TEAM(1, "组队模式", "组队"),
+    COUPLE(2, "双人模式", "双人"),
+    KEYBOARD(3, "键盘模式", "键盘"),
     ;
 
     private final int code;
     private final String desc;
+    private final String simpleName;
 
-    RoomModeEnum(int code, String desc) {
+    RoomModeEnum(int code, String desc, String simpleName) {
         this.code = code;
         this.desc = desc;
+        this.simpleName = simpleName;
     }
 
     public int getCode() {
@@ -27,6 +29,10 @@ public enum RoomModeEnum {
 
     public String getDesc() {
         return desc;
+    }
+
+    public String getSimpleName() {
+        return simpleName;
     }
 
     public static RoomModeEnum ofCode(int code, RoomModeEnum defaultValue) {
