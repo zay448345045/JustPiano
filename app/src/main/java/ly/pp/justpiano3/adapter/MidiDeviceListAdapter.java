66ebb20332a32a294baaf6ae1dabc09ba3585654
anshen <jiangrenjie.jrj@alibaba-inc.com>
2023-11-27 22:41:40 +0800

    midi设备控制文案优化

--- app/src/main/java/ly/pp/justpiano3/adapter/MidiDeviceListAdapter.java
+++ app/src/main/java/ly/pp/justpiano3/adapter/MidiDeviceListAdapter.java
@@ -64,15 +64,17 @@ public final class MidiDeviceListAdapter extends BaseAdapter {
             operateButton.setTag(midiDeviceInfoList[i]);
             if (MidiDeviceUtil.getAllConnectedMidiDeviceIdAndNameList().contains(
                     MidiDeviceUtil.getDeviceIdAndName(midiDeviceInfoList[i]))) {
-                enableText.setText("开启中");
+                enableText.setText("已启用");
                 enableText.setTextColor(Color.RED);
                 operateButton.setText("断开");
+                operateButton.setBackgroundResource(R.drawable.selector_ol_button_login);
                 operateButton.setOnClickListener(view1 -> new MidiDeviceListPreferenceTask(
                         this, midiDeviceInfoList[i]).execute(false));
             } else {
                 enableText.setText("已禁用");
                 enableText.setTextColor(Color.BLACK);
                 operateButton.setText("连接");
+                operateButton.setBackgroundResource(R.drawable.selector_ol_button);
                 operateButton.setOnClickListener(view1 -> new MidiDeviceListPreferenceTask(
                         this, midiDeviceInfoList[i]).execute(true));
             }
