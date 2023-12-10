f148b7d6ac2c5f79cfe263c6386026f8e9caa513
anshen <jiangrenjie.jrj@alibaba-inc.com>
2023-12-11 00:21:03 +0800

    反馈内容过长时不进行提交

--- app/src/main/java/ly/pp/justpiano3/task/FeedbackTask.java
+++ app/src/main/java/ly/pp/justpiano3/task/FeedbackTask.java
@@ -6,6 +6,7 @@ import android.os.AsyncTask;
 import android.widget.Toast;
 
 import java.lang.ref.WeakReference;
+import java.util.Objects;
 
 import io.netty.util.internal.StringUtil;
 import ly.pp.justpiano3.BuildConfig;
@@ -29,18 +30,19 @@ public final class FeedbackTask extends AsyncTask<Void, Void, String> {
 
     @Override
     protected String doInBackground(Void... v) {
+        if (message.length() >= 4000) {
+            return "invalid";
+        }
         String url = "http://" + OnlineUtil.server + ":8910/JustPianoServer/server/Feedback";
         FormBody.Builder formBuilder = new FormBody.Builder();
         formBuilder.add("version", BuildConfig.VERSION_NAME);
         formBuilder.add("userName", userName);
         formBuilder.add("message", message);
         RequestBody requestBody = formBuilder.build();
-
         Request request = new Request.Builder()
                 .url(url)
                 .post(requestBody)
                 .build();
-
         try (Response response = OkHttpUtil.client().newCall(request).execute()) {
             if (response.isSuccessful()) {
                 return response.body().string();
@@ -54,8 +56,13 @@ public final class FeedbackTask extends AsyncTask<Void, Void, String> {
     @Override
     protected void onPostExecute(String responseStr) {
         if (weakReference.get() instanceof Activity) {
-            ((Activity) weakReference.get()).runOnUiThread(() -> Toast.makeText(weakReference.get(),
-                    StringUtil.isNullOrEmpty(responseStr) ? "反馈提交出错" : "反馈提交成功", Toast.LENGTH_SHORT).show());
+            if (Objects.equals("invalid", responseStr)) {
+                ((Activity) weakReference.get()).runOnUiThread(() -> Toast.makeText(weakReference.get(),
+                        "反馈内容过长，无法提交", Toast.LENGTH_SHORT).show());
+            } else {
+                ((Activity) weakReference.get()).runOnUiThread(() -> Toast.makeText(weakReference.get(),
+                        StringUtil.isNullOrEmpty(responseStr) ? "反馈提交出错" : "反馈提交成功", Toast.LENGTH_SHORT).show());
+            }
         }
     }
 }
